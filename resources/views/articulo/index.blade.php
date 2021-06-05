f9480d3155c43d3a1c54549aea53c6ea7316c55f
jamv11 <j.a.m.v11@outlook.com>
2021-06-05 11:40:40 -0400

    Se realizó la configuración de Datatables

--- resources/views/articulo/index.blade.php
+++ resources/views/articulo/index.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.PlantillaBase')
 
+@section('css')
+
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css">
+
+@endsection
+
 @section('contenido')
 
-<a href="articulos/create" class="btn btn-primary mt-5">Crear</a>
+<a href="articulos/create" class="btn btn-primary mt-5 mb-5">Crear</a>
 
-<table class="table table-dark table-striped table-hover mt-4">
+<table id="articulos" class="table table-dark table-striped table-hover mt-4" style="width:100%">
  <thead>
     <tr>
         <th>ID</th>
@@ -16,7 +22,7 @@
     </tr>
  </thead>
 <tbody>
-    @foreach($articulos as $articulo);
+    @foreach($articulos as $articulo)
         <tr>
             <td>{{$articulo->id}}</td>
             <td>{{$articulo->codigo}}</td>
@@ -37,4 +43,24 @@
 </tbody>
 </table>
 
+@section('js')
+
+<script src = "https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src =  "https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+<script src = "https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap5.min.js"></script>
+<script >
+$(document).ready(function() {
+    $('#articulos').DataTable({
+
+        //Configuración de Paginacion del dataTables
+        "lengthMenu":[[5,10,50], [5,10,50, "All"]]
+    });
+        
+} );
+</script>
+
 @endsection
+
+@endsection
+
+
